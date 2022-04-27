22352d9e0529e8e711bcb470cf12e98579653f08
Dénes Berger <dberger@cloudera.com>
2022-04-27 13:45:21 +0200

    CB-16947 Take IV as parameter instead of fixed value

--- saltstack/base/salt/prerequisites/usr/bin/ccmv2-helper.sh
+++ saltstack/base/salt/prerequisites/usr/bin/ccmv2-helper.sh
@@ -5,6 +5,7 @@ setup_ccmv2() {
   : ${CCM_V2_AGENT_ENCIPHERED_KEY:? required}
   : ${CCM_V2_AGENT_KEY_ID:? required}
   : ${CCM_V2_AGENT_BACKEND_ID_PREFIX:? required}
+  : ${CCM_V2_IV:=436c6f7564657261436c6f7564657261}
 
   BACKEND_ID="${CCM_V2_AGENT_BACKEND_ID_PREFIX}${INSTANCE_ID}"
   BACKEND_HOST="localhost"
@@ -12,11 +13,10 @@ setup_ccmv2() {
 
   mkdir -p /etc/ccmv2
 
-  IV=436c6f7564657261436c6f7564657261
   AGENT_KEY_PATH=/etc/ccmv2/ccmv2-key.enc
 
   CCM_V2_AGENT_KEY_HEX=$(xxd -pu -l 16 <<< $CCM_V2_AGENT_KEY_ID)
-  echo ${CCM_V2_AGENT_ENCIPHERED_KEY} | openssl enc -aes-128-cbc -d -A -a -K ${CCM_V2_AGENT_KEY_HEX} -iv ${IV} > ${AGENT_KEY_PATH}
+  echo ${CCM_V2_AGENT_ENCIPHERED_KEY} | openssl enc -aes-128-cbc -d -A -a -K ${CCM_V2_AGENT_KEY_HEX} -iv ${CCM_V2_IV} > ${AGENT_KEY_PATH}
   chmod 400 "$AGENT_KEY_PATH"
 
   AGENT_CERT_PATH=/etc/ccmv2/ccmv2-cert.enc
@@ -34,7 +34,7 @@ setup_ccmv2() {
   if [[ "$IS_CCM_V2_JUMPGATE_ENABLED" == "true" && "$IS_FREEIPA" == "true" ]]; then
     AGENT_ENVIRONMENT_CRN="$ENVIRONMENT_CRN"
     ACCESS_KEY_ID="$CCM_V2_AGENT_ACCESS_KEY_ID"
-    ACCESS_KEY="$(echo ${CCM_V2_AGENT_ENCIPHERED_ACCESS_KEY} | openssl enc -aes-128-cbc -d -A -a -K ${CCM_V2_AGENT_KEY_HEX} -iv ${IV})"
+    ACCESS_KEY="$(echo ${CCM_V2_AGENT_ENCIPHERED_ACCESS_KEY} | openssl enc -aes-128-cbc -d -A -a -K ${CCM_V2_AGENT_KEY_HEX} -iv ${CCM_V2_IV})"
   fi
 
   # A more sophisticated solution might need to be patched in later - tbh the script originally expected a full url with protocol scheme and closing slash
